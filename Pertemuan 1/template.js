34a054a6a07073817e325ad8c9face606ea7505d
ahmadnurfauziii <1941720068@student.polinema.ac.id>
2022-02-14 10:07:17 +0700

    first commit

---
+++ Pertemuan 1/template.js
@@ -0,0 +1,11 @@
+let fname = `Christian`;
+let lname = `Ronaldo`;
+let age = prompt("Masukan umur Christian Ronaldo!");
+
+// Cara Lamaa
+// let result = fname + ' ' + lname + ' is ' + age + ' years old ' ;
+//alert(Result);
+
+//Memakai template strings
+let result = `${fname} ${lname} is ${age} years old`;
+alert (result);
